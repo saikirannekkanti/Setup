7c5d2780866e1ca6df8ca7e796d89253d7d79939
saikirannekkanti <83533778+saikirannekkanti@users.noreply.github.com>
2024-09-08 15:45:01 +0530

    Update linuxcommand.sh

--- linuxcommand.sh
+++ linuxcommand.sh
@@ -1,3 +1,37 @@
 #create file
 touch filename.extension
-cat 
+
+# view content in file (read)
+cat filename.txt
+
+#view+ editing and read file content interesst content on file i, save wq! or shift+zz
+Vi filename.txt
+vim filename.txt
+
+#create custom command as shortcut
+vim .bashrc
+# type alias gc =git clone
+source .bashrc
+
+
+#create folder
+mkdir foldername
+#delete entire directory or folder
+rmdir foldername
+
+#moving file to folder or any directory
+mv filename /foldername/
+
+#copy file to folder or any directory
+cp filename /foldername/
+
+#delete file 
+rm filename 
+#force delete
+rm filename -f
+rm -rf filename
+#rm= remove rf=recussive force
+
+
+
+
